0acd8177cb47af6d53f1a44badec8428a19e7aa7
PRAKAVI-K <k.prakavi1212@gmail.com>
2025-02-04 21:21:59 +0530

    updated

--- app.js
+++ app.js
@@ -1,12 +1,10 @@
-
 const toggleButton = document.getElementsByClassName("toggle-btn");
-const sidebar = document.getElementById('sidebar');
+const sidebar = document.getElementById("sidebar");
 
- function toggleSideBar(){
-      sidebar.classList.toggle("close");
-      toggleButton.classList.toggle("rotate");
- }
-
+function toggleSideBar() {
+  toggleButton.sidebar.classList.toggle("close");
+  toggleButton.classList.toggle("rotate");
+}
 
 function toggleSubMenu(button) {
   button.nextElementSibling.classList.toggle("show");
